69fad7416eb7a5225fc23f51f218f3ad59e9cd60
destinytaoer <1848765519@qq.com>
2019-03-06 23:26:55 +0800

    增加动态规划背包问题

--- src/dynamic-programming.js
+++ src/dynamic-programming.js
@@ -59,11 +59,12 @@ function lcs(str1, str2) {
   }
   // 构建用于保存字符串匹配记录的表
   for (let i = 0; i <= str1.length; i++) {
-    for (let j = 0; j <= str2.lenth; j++) {
+    for (let j = 0; j <= str2.length; j++) {
+      
       if (i === 0 || j === 0) {
         lcsArr[i][j] = 0;
       } else {
-        if (str[i - 1] === str2[j - 1]) {
+        if (str1[i - 1] === str2[j - 1]) {
           lcsArr[i][j] = lcsArr[i - 1][j - 1] + 1
         } else {
           lcsArr[i][j] = 0
@@ -79,8 +80,50 @@ function lcs(str1, str2) {
   // 确定从哪里开始构建最长公共子串
   let str = ''
   if (max === 0) return ""
-  for (let i = index - max; i <= max; i++) {
-    str += str2[i]
+  for (let i = index - max; i < index; i++) {
+    str += str1[i]
   }
   return str
 }
+
+console.log(lcs('abbcc', 'dbbcc'))
+function max(a, b) {
+  return a > b ? a : b
+}
+function recurPackage(content, sizes, values, n) {
+  if (n === 0 || content === 0) return 0
+  if (sizes[n - 1] > content) {
+    return recurPackage(content, sizes, values, n - 1)
+  } else {
+    return max(values[n - 1] + recurPackage(content - sizes[n - 1], sizes, values, n - 1), recurPackage(content, sizes, values, n - 1))
+  }
+}
+let sizes = [3, 4, 7, 8, 9]
+let values = [4, 5, 10, 11, 13]
+let content = 16
+let n = 5
+
+console.log(recurPackage(content, sizes, values, n));
+
+function dynPackage(content, sizes, values, n) {
+  let k = []
+  for (let i = 0; i <= n + 1; i++) {
+    k[i] = []
+  }
+
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= content; j++) {
+      if (i === 0 || j === 0) {
+        k[i][j] = 0
+      } else if (sizes[i - 1] <= j) {
+        k[i][j] = max(values[i - 1] + k[i - 1][j - sizes[i - 1]], k[i - 1][j])
+      } else {
+        k[i][j] = k[i - 1][j]
+      }
+    }
+    console.log(k[i]);
+  }
+  return k[n][content]
+}
+
+console.log(dynPackage(content, sizes, values, n));
